027d3f49829c7848e49903fc2ec578f658e03571
nnam24 <97780933+nnam24@users.noreply.github.com>
2024-12-30 18:14:07 +0700

    Build Repo interface

---
+++ modules/analytics/repository/time_progress_repo_interface.go
@@ -0,0 +1,10 @@
+package repository
+
+import "backend-golang/modules/analytics/domain/entity"
+
+type TimeProgressReaderRepo interface {
+	GetTotalTimeSpent(userId int, startTime string, endTime string) (string, error)
+	GetTimeSpentDaily(userId int, startTime string, endTime string) (*[]entity.DailyProgressEntity, error)
+	GetEachTaskProgress(userId int, startTime string, endTime string) (*[]entity.TaskProgressEntity, error)
+	GetTaskNumberByStatus(userId int, startTime string, endTime string) (*[]entity.TaskNumberByStatusEntity, error)
+}
