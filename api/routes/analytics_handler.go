b9ead624356e6d6603974c1b1c93afe9b1c0775f
nnam24 <97780933+nnam24@users.noreply.github.com>
2024-12-30 18:11:49 +0700

    Add route for analytics

---
+++ api/routes/analytics_handler.go
@@ -0,0 +1,42 @@
+package routes
+
+import (
+	"backend-golang/api/middlewares"
+	analyticsHandler "backend-golang/modules/analytics/api/handler"
+	"backend-golang/modules/analytics/domain/usecase"
+	"backend-golang/modules/analytics/repository"
+	database "backend-golang/pkgs/dbs/postgres"
+	"backend-golang/pkgs/transport/http/method"
+	"backend-golang/pkgs/transport/http/route"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/openai/openai-go"
+)
+
+func NewAnalyticsHandler(db *database.Database, openaiClient *openai.Client, requestValidator *validator.Validate) analyticsHandler.AnalyticsHandler {
+	timeProgressReaderRepo := repository.NewTimeProgressReaderRepository(*db)
+
+	return analyticsHandler.NewAnalyticsHandler(requestValidator,
+		usecase.NewGetUserProgressUsecase(timeProgressReaderRepo),
+		usecase.NewGetRatioTotalTimeSpentUsecase(timeProgressReaderRepo),
+		usecase.NewGetTimeSpentDailyUsecase(timeProgressReaderRepo),
+		usecase.NewGetTaskOfEachStatusUsecase(timeProgressReaderRepo),
+		usecase.NewGetAIFeedbackUsecase(timeProgressReaderRepo),
+	)
+}
+
+func (r *RouteHandler) analyticsRoute() route.GroupRoute {
+	return route.GroupRoute{
+		Prefix: "/api/v1/analytics",
+		Routes: []route.Route{
+			{
+				Path:    "/daily-spent-time",
+				Method:  method.POST,
+				Handler: r.AnalyticsHandler.HandleGetTimeSpentDaily,
+				Middlewares: route.Middlewares(
+					middlewares.Authentication(),
+				),
+			},
+		},
+	}
+}
--- api/routes/route_handler.go
+++ api/routes/route_handler.go
@@ -1,20 +1,23 @@
 package routes
 
 import (
+	analyticsHandler "backend-golang/modules/analytics/api/handler"
 	taskHandler "backend-golang/modules/task/api/handler"
 	userHandler "backend-golang/modules/user/api/handler"
 	"backend-golang/pkgs/transport/http/route"
 )
 
 type RouteHandler struct {
-	UserHandler userHandler.UserHandler
-	TaskHandler taskHandler.TaskHandler
+	UserHandler      userHandler.UserHandler
+	TaskHandler      taskHandler.TaskHandler
+	AnalyticsHandler analyticsHandler.AnalyticsHandler
 }
 
 func (r *RouteHandler) InitGroupRoutes() []route.GroupRoute {
 	var routeGroup []route.GroupRoute
 	routeGroup = append(routeGroup, r.userRoute())
 	routeGroup = append(routeGroup, r.taskRoute())
+	routeGroup = append(routeGroup, r.analyticsRoute())
 
 	return routeGroup
 }
